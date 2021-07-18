d65627f3f0b8a2d605b328fd5775f4eba929676c
Pedrolustosa <pedroeternalss@gmail.com>
2021-07-17 23:09:14 -0300

    Evento Controller

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Value";
+        }
+    }
+}
