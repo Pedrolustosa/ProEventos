0775af252558efb85669f2984bbc38a1230e09ae
Pedrolustosa <pedroeternalss@gmail.com>
2021-07-19 19:05:27 -0300

    Evento Model

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
 {
@@ -10,14 +11,59 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+        public IEnumerable<Evento> _evento = new Evento[]{
+        new Evento(){
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "Belo Horizonte",
+                Lote = "1° Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImageURL = "foto1.png"
+            },
+            new Evento(){
+                EventoId = 2,
+                Tema = "Angular 11 e .NET 5",
+                Local = "Belo Horizonte",
+                Lote = "1° Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImageURL = "foto2.png"
+            }
+        };
+
         public EventoController()
         {
         }
 
         [HttpGet]
-        public string Get()
+        public IEnumerable<Evento> Get()
         {
-            return "Value";
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetByID(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Post";
+        }
+
+        [HttpPut]
+        public string Put()
+        {
+            return "Put";
+        }
+
+        [HttpDelete]
+        public string Delete()
+        {
+            return "delete";
         }
     }
 }
