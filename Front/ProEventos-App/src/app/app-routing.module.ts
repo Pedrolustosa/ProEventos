b6360a0054e6c58bd60d93bbad17108f49a0c768
Pedrolustosa <pedroeternalss@gmail.com>
2021-07-20 22:36:21 -0300

    Grid dos Eventos + Route

--- Front/ProEventos-App/src/app/app-routing.module.ts
+++ Front/ProEventos-App/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: EventosComponent},
+  {path: 'eventos', component: EventosComponent},
+  {path: 'palestrantes', component: PalestrantesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
