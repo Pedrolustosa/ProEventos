2fba1b73271c44575691162efb2aa10a7dea14d9
Pedro Lustosa <pedroeternalss@gmail.com>
2021-07-22 16:58:36 -0300

    Filtrando Grid

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
   public eventos: any = [];
+  filteredEvents: any = [];
   showImage = true;
   private _filterList: string = '';
 
@@ -17,7 +18,7 @@ export class EventosComponent implements OnInit {
 
   public set filterList(value: string){
     this._filterList = value;
-    this.eventos = this.filterList ? this.filterEvents(this.filterList) : this.eventos;
+    this.filteredEvents = this.filterList ? this.filterEvents(this.filterList) : this.eventos;
   }
 
   filterEvents(filterFor: string): any{
@@ -39,7 +40,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.filteredEvents = this.eventos;
+      },
       error => console.log(error)
     );
   }
